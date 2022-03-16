b7fce268a6e97ff4dcb8c980b11c2c08609a80a4
tingtt <ting.taku@gmail.com>
2022-03-16 20:01:05 +0900

    [add] /changelogs/{id} DELETE

--- changelog/delete.go
+++ changelog/delete.go
@@ -1,5 +1,35 @@
 package changelog
 
+import "flow-records/mysql"
+
 func Delete(userId uint64, id uint64) (notFound bool, err error) {
+	db, err := mysql.Open()
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	stmtIns, err := db.Prepare("DELETE FROM changelogs WHERE user_id = ? AND id = ?")
+	if err != nil {
+		return
+	}
+	defer stmtIns.Close()
+
+	result, err := stmtIns.Exec(userId, id)
+	if err != nil {
+		return
+	}
+
+	// Check affected row count
+	affectedRowCount, err := result.RowsAffected()
+	if err != nil {
+		return
+	}
+	if affectedRowCount == 0 {
+		// Not found
+		notFound = true
+		return
+	}
+
 	return
 }
