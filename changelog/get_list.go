bf13f5def6ea358d1f33cfaa0d79d996463f07fd
tingtt <ting.taku@gmail.com>
2022-03-16 16:25:35 +0900

    [add] /changelogs GET

--- changelog/get_list.go
+++ changelog/get_list.go
@@ -1,14 +1,70 @@
 package changelog
 
-import "time"
+import (
+	"flow-records/mysql"
+	"time"
+)
 
 type GetListQuery struct {
-	ProjectId *uint64    `query:"project_id" validate:"omitempty,gte=1"`
 	Start     *time.Time `query:"start" validate:"omitempty"`
 	End       *time.Time `query:"end" validate:"omitempty"`
-	Embed     *string    `query:"embed" validate:"omitempty,oneof=records record.changelog"`
+	TodoId    *uint64    `query:"todo_id" validate:"omitempty,gte=1"`
+	SchemeId  *uint64    `query:"scheme_id" validate:"omitempty,gte=1"`
+	ProjectId *uint64    `query:"project_id" validate:"omitempty,gte=1"`
 }
 
 func GetList(userId uint64, q GetListQuery) (changeLogs []ChangeLog, err error) {
+	// Generate query
+	queryStr := "SELECT id, text, datetime, todo_id, scheme_id FROM changelogs WHERE user_id = ?"
+	queryParams := []interface{}{userId}
+	if q.Start != nil {
+		queryStr += " AND datetime >= ?"
+		queryParams = append(queryParams, q.Start.UTC())
+	}
+	if q.End != nil {
+		queryStr += " AND datetime >= ?"
+		queryParams = append(queryParams, q.End.UTC())
+	}
+	if q.TodoId != nil {
+		queryStr += " AND todo_id = ?"
+		queryParams = append(queryParams, *q.TodoId)
+	}
+	if q.SchemeId != nil {
+		queryStr += " AND scheme_id = ?"
+		queryParams = append(queryParams, *q.SchemeId)
+	}
+	if q.ProjectId != nil {
+		queryStr += ` AND scheme_id IN (
+			SELECT id FROM schemes WHERE project_id = ?
+		)`
+		queryParams = append(queryParams, *q.ProjectId)
+	}
+
+	db, err := mysql.Open()
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	stmtOut, err := db.Prepare(queryStr)
+	if err != nil {
+		return
+	}
+	defer stmtOut.Close()
+
+	rows, err := stmtOut.Query(queryParams...)
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		c := ChangeLog{}
+		err = rows.Scan(&c.Id, &c.Text, &c.Datetime, &c.TodoId, &c.SchemeId)
+		if err != nil {
+			return
+		}
+
+		changeLogs = append(changeLogs, c)
+	}
 	return
 }
--- handler_changelogs_get_list.go
+++ handler_changelogs_get_list.go
@@ -18,7 +18,27 @@ func changeLogGetList(c echo.Context) error {
 		return c.JSONPretty(http.StatusUnauthorized, map[string]string{"message": err.Error()}, "	")
 	}
 
-	changeLogs, err := changelog.GetList(userId, changelog.GetListQuery{})
+	// Bind request query
+	query := new(changelog.GetListQuery)
+	if err = c.Bind(query); err != nil {
+		// 400: Bad request
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
+	}
+
+	// Validate request query
+	if err = c.Validate(query); err != nil {
+		// 400: Bad request
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
+	}
+	if query.Start != nil && query.End != nil && query.Start.After(*query.End) {
+		// 400: Bad request
+		c.Logger().Debug("`start` must before `end`")
+		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": "`start` must before `end`"}, "	")
+	}
+
+	changeLogs, err := changelog.GetList(userId, *query)
 	if err != nil {
 		// 500: Internal server error
 		c.Logger().Debug(err)
@@ -26,5 +46,8 @@ func changeLogGetList(c echo.Context) error {
 	}
 
 	// 200: Success
+	if changeLogs == nil {
+		return c.JSONPretty(http.StatusOK, []interface{}{}, "	")
+	}
 	return c.JSONPretty(http.StatusOK, changeLogs, "	")
 }
