07c6e8467ea99a56eded28bcec94e018df5cd771
tingtt <ting.taku@gmail.com>
2022-03-16 16:12:48 +0900

    [change] 415 -> 400 GET処理のクエリパラメータのバリデーションエラー

--- handler_schemes_get_list.go
+++ handler_schemes_get_list.go
@@ -28,9 +28,9 @@ func schemeGetList(c echo.Context) error {
 
 	// Validate request query
 	if err = c.Validate(query); err != nil {
-		// 422: Unprocessable entity
+		// 400: Bad request
 		c.Logger().Debug(err)
-		return c.JSONPretty(http.StatusUnprocessableEntity, map[string]string{"message": err.Error()}, "	")
+		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
 	}
 
 	schemes, err := scheme.GetList(userId, *query)
