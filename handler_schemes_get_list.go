8411c8dbf6edc99deab236117b791cf21092057c
tingtt <ting.taku@gmail.com>
2022-03-16 16:10:37 +0900

    [clean] 変数名

--- handler_schemes_get_list.go
+++ handler_schemes_get_list.go
@@ -18,22 +18,22 @@ func schemeGetList(c echo.Context) error {
 		return c.JSONPretty(http.StatusUnauthorized, map[string]string{"message": err.Error()}, "	")
 	}
 
-	// Bind request body
-	post := new(scheme.GetListQuery)
-	if err = c.Bind(post); err != nil {
+	// Bind request query
+	query := new(scheme.GetListQuery)
+	if err = c.Bind(query); err != nil {
 		// 400: Bad request
 		c.Logger().Debug(err)
 		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
 	}
 
-	// Validate request body
-	if err = c.Validate(post); err != nil {
+	// Validate request query
+	if err = c.Validate(query); err != nil {
 		// 422: Unprocessable entity
 		c.Logger().Debug(err)
 		return c.JSONPretty(http.StatusUnprocessableEntity, map[string]string{"message": err.Error()}, "	")
 	}
 
-	schemes, err := scheme.GetList(userId, *post)
+	schemes, err := scheme.GetList(userId, *query)
 	if err != nil {
 		// 500: Internal server error
 		c.Logger().Debug(err)
