945416f57f066387d5e4b1f1e0f19386cf65642d
tingtt <ting.taku@gmail.com>
2022-03-16 19:59:07 +0900

    [add] /changelogs/{id} PATCH

--- changelog/patch.go
+++ changelog/patch.go
@@ -1,11 +1,55 @@
 package changelog
 
+import (
+	"flow-records/mysql"
+	"time"
+)
+
 type PatchBody struct {
-	Name      *string `json:"name" validate:"omitempty,gte=1"`
-	SumGraph  *bool   `json:"sum_graph" validate:"omitempty"`
-	ProjectId *uint64 `json:"project_id" validate:"omitempty,gte=1"`
+	Text     *string    `json:"text" validate:"required,gte=1"`
+	Datetime *time.Time `json:"datetime" validate:"omitempty"`
+	SchemeId *uint64    `json:"scheme_id" validate:"omitempty,gte=1"`
 }
 
 func Patch(userId uint64, id uint64, p PatchBody) (c ChangeLog, notFound bool, err error) {
+	old, notFound, err := Get(userId, id)
+	if err != nil {
+		return
+	}
+	if notFound {
+		return
+	}
+
+	if p.Text == nil {
+		p.Text = &old.Text
+	}
+	if p.Datetime == nil {
+		p.Datetime = &old.Datetime
+	}
+	if p.SchemeId == nil {
+		p.SchemeId = &old.SchemeId
+	}
+
+	// Update row
+	db, err := mysql.Open()
+	if err != nil {
+		return
+	}
+	defer db.Close()
+	stmtIns, err := db.Prepare("UPDATE changelogs SET text = ?, datetime = ?, scheme_id = ? WHERE user_id = ? AND id = ?")
+	if err != nil {
+		return
+	}
+	defer stmtIns.Close()
+	_, err = stmtIns.Exec(p.Text, p.Datetime, p.SchemeId, userId, id)
+	if err != nil {
+		return
+	}
+
+	c.Id = id
+	c.Text = *p.Text
+	c.Datetime = *p.Datetime
+	c.TodoId = old.TodoId
+	c.SchemeId = *p.SchemeId
 	return
 }
--- handler_changelogs_patch.go
+++ handler_changelogs_patch.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flow-records/changelog"
 	"flow-records/jwt"
+	"flow-records/scheme"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -34,7 +36,38 @@ func changeLogPatch(c echo.Context) error {
 		return echo.ErrNotFound
 	}
 
-	cl, notFound, err := changelog.Patch(userId, id, changelog.PatchBody{})
+	// Bind request body
+	post := new(changelog.PatchBody)
+	if err = c.Bind(post); err != nil {
+		// 400: Bad request
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
+	}
+
+	// Validate request body
+	if err = c.Validate(post); err != nil {
+		// 422: Unprocessable entity
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusUnprocessableEntity, map[string]string{"message": err.Error()}, "	")
+	}
+
+	// Check schemeId
+	if post.SchemeId != nil {
+		_, notFound, err := scheme.Get(userId, *post.SchemeId, scheme.GetQuery{})
+		if err != nil {
+			// 500: Internal server error
+			c.Logger().Debug(err)
+			return c.JSONPretty(http.StatusInternalServerError, map[string]string{"message": err.Error()}, "	")
+		}
+		if notFound {
+			// 409: Conflict
+			c.Logger().Debug(fmt.Sprintf("scheme id: %d does not exists", post.SchemeId))
+			return c.JSONPretty(http.StatusConflict, map[string]string{"message": fmt.Sprintf("scheme id: %d does not exists", post.SchemeId)}, "	")
+		}
+	}
+
+	// Write to db
+	cl, notFound, err := changelog.Patch(userId, id, *post)
 	if err != nil {
 		// 500: Internal server error
 		c.Logger().Debug(err)
