177c139a1de1a6372e61e9ffa4808564cb6c5c5e
tingtt <ting.taku@gmail.com>
2022-03-17 12:41:59 +0900

    [add] / DELETE

--- record/delete.go
+++ record/delete.go
@@ -1,5 +1,34 @@
 package record
 
+import "flow-records/mysql"
+
 func Delete(userId uint64, id uint64) (notFound bool, err error) {
+	db, err := mysql.Open()
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	stmtIns, err := db.Prepare("DELETE FROM records WHERE user_id = ? AND id = ?")
+	if err != nil {
+		return
+	}
+	defer stmtIns.Close()
+
+	result, err := stmtIns.Exec(userId, id)
+	if err != nil {
+		return
+	}
+
+	// Check affected row count
+	affectedRowCount, err := result.RowsAffected()
+	if err != nil {
+		return
+	}
+	if affectedRowCount == 0 {
+		// Not found
+		notFound = true
+		return
+	}
 	return
 }
