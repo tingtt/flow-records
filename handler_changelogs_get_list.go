08bab2ee1ec65f346951b88d7c9538bec8eae91b
tingtt <ting.taku@gmail.com>
2022-03-17 12:18:35 +0900

    [update] /changelogs GET (query)

--- handler_changelogs_get_list.go
+++ handler_changelogs_get_list.go
@@ -4,10 +4,19 @@ import (
 	"flow-records/changelog"
 	"flow-records/jwt"
 	"net/http"
+	"time"
 
 	jwtGo "github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 )
+
+type GetChangeLogListQuery struct {
+	Start     *string `query:"start" validate:"omitempty,datetime"`
+	End       *string `query:"end" validate:"omitempty,datetime"`
+	TodoId    *uint64 `query:"todo_id" validate:"omitempty,gte=1"`
+	SchemeId  *uint64 `query:"scheme_id" validate:"omitempty,gte=1"`
+	ProjectId *uint64 `query:"project_id" validate:"omitempty,gte=1"`
+}
 
 func changeLogGetList(c echo.Context) error {
 	// Check token
@@ -19,7 +28,7 @@ func changeLogGetList(c echo.Context) error {
 	}
 
 	// Bind request query
-	query := new(changelog.GetListQuery)
+	query := new(GetChangeLogListQuery)
 	if err = c.Bind(query); err != nil {
 		// 400: Bad request
 		c.Logger().Debug(err)
@@ -32,13 +41,28 @@ func changeLogGetList(c echo.Context) error {
 		c.Logger().Debug(err)
 		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
 	}
-	if query.Start != nil && query.End != nil && query.Start.After(*query.End) {
-		// 400: Bad request
-		c.Logger().Debug("`start` must before `end`")
-		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": "`start` must before `end`"}, "	")
+	var start, end *time.Time
+	if query.Start != nil {
+		startTmp, err := datetimeStrConv(*query.Start)
+		if err != nil {
+			// 400: Bad request
+			c.Logger().Debug(err)
+			return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
+		}
+		start = &startTmp
 	}
+	if query.End != nil {
+		endTmp, err := datetimeStrConv(*query.End)
+		if err != nil {
+			// 400: Bad request
+			c.Logger().Debug(err)
+			return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
+		}
+		end = &endTmp
+	}
+	queryParsed := changelog.GetListQuery{Start: start, End: end, TodoId: query.TodoId, SchemeId: query.SchemeId, ProjectId: query.ProjectId}
 
-	changeLogs, err := changelog.GetList(userId, *query)
+	changeLogs, err := changelog.GetList(userId, queryParsed)
 	if err != nil {
 		// 500: Internal server error
 		c.Logger().Debug(err)
