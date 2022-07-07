dee46b5412b58a9f99e85d60a923ad9d791b3666
tingtt <ting.taku@gmail.com>
2022-07-07 18:43:21 +0900

    [add] health check route and external servicies health check

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flow-records/jwt"
 	"flow-records/mysql"
 	"fmt"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
@@ -97,13 +98,48 @@ func main() {
 	e.Logger.SetLevel(log.Lvl(*logLevel))
 	e.Validator = &CustomValidator{validator: validator.New()}
 
+	// Setup db client instance
+	e.Logger.Info(mysql.SetDSNTCP(*mysqlUser, *mysqlPasswd, *mysqlHost, *mysqlPort, *mysqlDB))
+	// Check connection
+	d, err := mysql.Open()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	if err = d.Ping(); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	// Service status check
+	if *serviceUrlProjects == "" {
+		e.Logger.Fatal("`--service-url-projects` option is required")
+	}
+	if ok, err := checkHealth(*serviceUrlProjects + "/-/readiness"); err != nil {
+		e.Logger.Fatalf("failed to check health of external service `flow-projects` %s", err)
+	} else if !ok {
+		e.Logger.Fatal("failed to check health of external service `flow-projects`")
+	}
+	if *serviceUrlTodos == "" {
+		e.Logger.Fatal("`--service-url-todos` option is required")
+	}
+	if ok, err := checkHealth(*serviceUrlTodos + "/-/readiness"); err != nil {
+		e.Logger.Fatalf("failed to check health of external service `flow-todos` %s", err)
+	} else if !ok {
+		e.Logger.Fatal("failed to check health of external service `flow-todos`")
+	}
+
+	// Setup JWT
 	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
 		Claims:     &jwt.JwtCustumClaims{},
 		SigningKey: []byte(*jwtSecret),
+		Skipper: func(c echo.Context) bool {
+			return c.Path() == "/-/readiness"
+		},
 	}))
 
-	// Setup db client instance
-	e.Logger.Info(mysql.SetDSNTCP(*mysqlUser, *mysqlPasswd, *mysqlHost, *mysqlPort, *mysqlDB))
+	// Health check route
+	e.GET("/-/readiness", func(c echo.Context) error {
+		return c.String(http.StatusOK, "flow-records is Healthy.\n")
+	})
 
 	// Restricted routes
 	e.POST("/", post)
